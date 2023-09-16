bb00a5fe6aa10a2c80bff48289ff54a1241df201
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-09-16 19:47:49 +0300

    add second_lab

---
+++ second_lab/Primes.java
@@ -0,0 +1,18 @@
+public class Primes {
+    public static void main(String[] args) {
+        for (int j = 2; j <=100; j++) {
+            if (isPrime(j) == true) {
+                System.out.println(j);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    } 
+} 
